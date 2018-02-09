26e4c0249def541544db21aa22129bc19cfa3e2b
Cocder <radiantstar123@yandex.com>
2018-02-09 21:40:19 +0000

    Delete pascal.cpp

--- pascal.cpp
+++
@@ -1,7 +0,0 @@
-ll pasc[22][22];
-for(int i=0;i<=20;i++){pasc[i][0]=1;}
-	for(int i=1;i<=20;i++){
-		for(int j=1;j<=i;j++){
-			pasc[i][j]=((tll)pasc[i-1][j-1]+pasc[i-1][j]);//c(n,k)=c(n-1,k-1)+c(n-1,k)
-		}
-	}
